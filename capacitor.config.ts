05db9ce8fa3ffc2006ff404c7d8475d516477426
moses-ramoeletsi <mramoeletsi14@gmail.com>
2024-08-30 22:32:24 +0200

    first commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'lesotho_wetlands',
+  webDir: 'www'
+};
+
+export default config;
